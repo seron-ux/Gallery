9b710cfce0292cbc099d396b95fe5172f0676831
seron-ux <abyseronkirui@gmail.com>
2021-03-20 17:40:39 +0300

    created the models

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    caption = models.CharField(max_length=100)
+    title = models.CharField(max_length=200, default='')
+    description = models.TextField(default='')
+    location = models.TextField(max_length=30,null=False,blank=False, default='')
+    image=models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField(auto_now = True)
