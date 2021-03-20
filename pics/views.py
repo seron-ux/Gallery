f730e9d7fae6e892b21c9f9c0913650dd1fe5ab9
seron-ux <abyseronkirui@gmail.com>
2021-03-20 21:53:26 +0300

    query all objects

--- pics/views.py
+++ pics/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    images = Image.objects.all()
+    print(images)
+    return render(request, 'home.html',{"images":images})
