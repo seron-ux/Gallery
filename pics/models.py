c964c1685eb7dc4031d43924aa30b3e9c1e95ede
seron-ux <abyseronkirui@gmail.com>
2021-03-21 14:12:05 +0300

    upload images

--- pics/models.py
+++ pics/models.py
@@ -4,9 +4,8 @@ from django.db import models
 class Image(models.Model):
     caption = models.CharField(max_length=100)
     title = models.CharField(max_length=200, default='')
-    description = models.TextField(default='')
     location = models.TextField(max_length=30,null=False,blank=False, default='')
-    image=models.ImageField(upload_to='images/')
+    image=models.ImageField(upload_to='images/',default='image.jpg')
     pub_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
