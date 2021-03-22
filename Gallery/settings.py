e867dffad6ca8a54cbe49f3ea504b87222d3ccc4
seron-ux <abyseronkirui@gmail.com>
2021-03-22 13:08:29 +0300

    added a README.md file

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -12,6 +12,38 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+
+MODE=config("MODE", default="dev")
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+# development
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,10 +151,22 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
@@ -132,3 +176,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+django_heroku.settings(locals())
---
+++ README.md
@@ -0,0 +1,52 @@
+# GALLERY 
+
+## Author
+
+Abigael chepkemoi
+
+## Description
+
+This is an app that displays different kind of photos. The user can search for an image of his or her interest using the  search category. The user can also copy the link of an image and share with his or her friends.
+
+
+## Setup Installation
+
+1. Clone or download and unzip the repository from github
+https://github.com/seron-ux/Gallery.git
+
+2. You can use this command https://github.com/seron-ux/Gallery.git to clone the project to your machine if you have git.
+
+3. cd into the project folder and open in text editor either code. or atom.
+
+4. Install all the requirements using this command:
+
+- pip install -r requirements.txt
+
+5. Run python3 manage.py runserver to launch the app
+
+## Technologies Used
+
+- Python3
+
+- Django3
+
+- Heroku
+
+- Git
+
+## Bugs
+
+There is no known bug in the application at the moment
+
+
+
+## License
+
+This project is licensed under the MIT LICENSE
+
+## Contact Details
+
+If you have any suggestion or modification that can better this project, feel free to contact me. Here are my details:
+
+- Abigael chepkemoi
+- <https://www.google.com/abyseronkirui@gmail.com>
